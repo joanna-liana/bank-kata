fb44d8f10f726b60d21a0ced445fad74382e7787
Joanna Liana <joanna_liana@outlook.com>
2022-01-16 19:45:20 +0100

    Fix formatting

--- src/account.test.ts
+++ src/account.test.ts
@@ -10,59 +10,50 @@ import {
   TransactionsRepository
 } from './TransactionsRepository';
 
-describe(
-  'Account',
-  () => {
-    let clock: Clock;
-    let print: PrintHandler;
-    let transactionsRepo: TransactionsRepository;
-    let account: Account;
+describe('Account', () => {
+  let clock: Clock;
+  let print: PrintHandler;
+  let transactionsRepo: TransactionsRepository;
+  let account: Account;
 
-    beforeEach(() => {
-      clock = { currentDate: new Date() };
+  beforeEach(() => {
+    clock = { currentDate: new Date() };
 
-      print = jest.fn();
-      transactionsRepo = new InMemoryTransactionsRepository(clock);
+    print = jest.fn();
+    transactionsRepo = new InMemoryTransactionsRepository(clock);
 
-      account = new Account(
-        transactionsRepo,
-        print,
-      );
-    });
-
-    // TODO: remove getAll
-    it(
-      'allows to deposit money',
-      async () => {
-        // when
-        clock.currentDate = new Date('2012-01-10');
-        account.deposit(100);
-
-        // then
-        const transactions = await transactionsRepo.getAll();
-
-        expect(transactions).toStrictEqual([{
-          date: new Date('2012-01-10'),
-          amount: 100
-        }]);
-      }
+    account = new Account(
+      transactionsRepo,
+      print,
     );
+  });
 
-    it(
-      'allows to withdraw money',
-      async () => {
-        // when
-        clock.currentDate = new Date('2012-01-12');
-        account.withdraw(100);
+  // TODO: remove getAll
+  it('allows to deposit money', async () => {
+    // when
+    clock.currentDate = new Date('2012-01-10');
+    account.deposit(100);
 
-        // then
-        const transactions = await transactionsRepo.getAll();
+    // then
+    const transactions = await transactionsRepo.getAll();
 
-        expect(transactions).toStrictEqual([{
-          date: new Date('2012-01-12'),
-          amount: -100
-        }]);
-      }
-    );
-  }
-);
+    expect(transactions).toStrictEqual([{
+      date: new Date('2012-01-10'),
+      amount: 100
+    }]);
+  });
+
+  it('allows to withdraw money', async () => {
+    // when
+    clock.currentDate = new Date('2012-01-12');
+    account.withdraw(100);
+
+    // then
+    const transactions = await transactionsRepo.getAll();
+
+    expect(transactions).toStrictEqual([{
+      date: new Date('2012-01-12'),
+      amount: -100
+    }]);
+  });
+});
